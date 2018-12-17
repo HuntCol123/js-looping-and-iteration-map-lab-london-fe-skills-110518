e4fe8ddde0123988c0c611d4fe6b037ef5b46781
Hunt Collings <hunt@collings.co.uk>
2018-12-17 17:58:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ return lowerCaseNames;
 
 
 
+<<<<<<< HEAD
 function nameToAttributes(drivers) {
   
 let someArray = drivers.map(function createNameObject(driverName) {
@@ -29,4 +30,16 @@ let someString = drivers.map(function stringify(drivers) {
 })
 
   return(someString);
+=======
+
+
+
+
+function nameToAttributes(){
+  let object = {}; 
+  
+  lowerCaseNames["firstName"] =
+  lowerCaseNames["lastName"] =
+  
+>>>>>>> 30c81527d8b9001ed1375cf7d61bd5bcde8fbb93
 }
